2e5a66767e04516256412a430cc8de785999f4c5
virjar <weijia.deng@qunar.com>
2016-10-08 17:15:25 +0800

    post方式下载server数据,防止url被截断

--- client/src/main/java/com/virjar/ipproxy/avaliabletest/Main.java
+++ client/src/main/java/com/virjar/ipproxy/avaliabletest/Main.java
@@ -1,12 +1,9 @@
 package com.virjar.ipproxy.avaliabletest;
 
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.protocol.BasicHttpContext;
+import java.util.List;
 
 import com.alibaba.fastjson.JSONObject;
-import com.virjar.ipproxy.httpclient.HttpInvoker;
-import com.virjar.ipproxy.ippool.IpPool;
-import com.virjar.ipproxy.util.PoolUtil;
+import com.virjar.ipproxy.ippool.strategy.resource.DefaultResourceFacade;
 import com.virjar.model.AvProxy;
 
 /**
@@ -14,15 +11,16 @@ import com.virjar.model.AvProxy;
  */
 public class Main {
     public static void main(String[] args) {
-        HttpClientContext httpClientContext = HttpClientContext.adapt(new BasicHttpContext());
-
-        for (int i = 0; i < 1000; i++) {
-            String quatity = HttpInvoker.getQuiet("http://www.66ip.cn/3.html", httpClientContext);
-            System.out.println(quatity);
-            AvProxy bindProxy = PoolUtil.getBindProxy(httpClientContext);
-            System.out.println(JSONObject.toJSONString(bindProxy));
-            PoolUtil.cleanProxy(httpClientContext);
-        }
-        IpPool.getInstance().destroy();
-    }
+        /*
+         * HttpClientContext httpClientContext = HttpClientContext.adapt(new BasicHttpContext()); for (int i = 0; i <
+         * 1000; i++) { String quatity = HttpInvoker.getQuiet("http://www.66ip.cn/3.html", httpClientContext);
+         * System.out.println(quatity); AvProxy bindProxy = PoolUtil.getBindProxy(httpClientContext);
+         * System.out.println(JSONObject.toJSONString(bindProxy)); PoolUtil.cleanProxy(httpClientContext); }
+         * IpPool.getInstance().destroy();
+         */
+        DefaultResourceFacade defaultResourceFacade = new DefaultResourceFacade();
+        List<AvProxy> avProxies = defaultResourceFacade.importProxy("www.66ip.cn", "http://www.66ip.cn/3.html", 10);
+        System.out.println(JSONObject.toJSONString(avProxies));
+        avProxies = defaultResourceFacade.importProxy("www.66ip.cn", "http://www.66ip.cn/3.html", 10);
+        System.out.println(JSONObject.toJSONString(avProxies)); }
 }
