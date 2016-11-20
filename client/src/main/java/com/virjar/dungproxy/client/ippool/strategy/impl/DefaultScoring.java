b2f2a4bfbdf8ff134b697aa94c6c023926b8110c
virjar <weijia.deng@qunar.com>
2016-11-20 18:35:19 +0800

    加日志,排查打分错误问题

--- client/src/main/java/com/virjar/dungproxy/client/ippool/strategy/impl/DefaultScoring.java
+++ client/src/main/java/com/virjar/dungproxy/client/ippool/strategy/impl/DefaultScoring.java
@@ -1,5 +1,8 @@
 package com.virjar.dungproxy.client.ippool.strategy.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.virjar.dungproxy.client.ippool.strategy.Scoring;
 import com.virjar.dungproxy.client.model.Score;
 
@@ -7,12 +10,17 @@ import com.virjar.dungproxy.client.model.Score;
  * Created by virjar on 16/11/16.
  */
 public class DefaultScoring implements Scoring {
+    private static final Logger logger = LoggerFactory.getLogger(DefaultScoring.class);
+
     @Override
     public double newAvgScore(Score score, int factory, boolean isSuccess) {
+        logger.info("old Score:{}  factory:{}", score.getAvgScore(), factory);
         if (score.getAvgScore() == 0D && isSuccess) {
             return 1D;
         }
         double newScore = isSuccess ? 1D : 0D;
-        return (score.getAvgScore() * (factory - 1) + newScore) / factory;
+        double ret = (score.getAvgScore() * (factory - 1) + newScore) / factory;
+        logger.info("newScore :{}", ret);
+        return ret;
     }
 }
