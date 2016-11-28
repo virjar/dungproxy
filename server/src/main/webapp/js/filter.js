31b897990a574c953fdc94590852d399f3ea0206
virjar <weijia.deng@qunar.com>
2016-11-28 21:22:30 +0800

    前端展示,休眠间隔错误

--- server/src/main/webapp/js/filter.js
+++ server/src/main/webapp/js/filter.js
@@ -3,7 +3,7 @@ var allFilter = angular.module('allFilter',[]);
 allFilter.filter('hibdate',function() {
 	return function(input) {
 		if(input) {
-			return input/60000+'m';
+			return input+'m';
 		}
 	}
 });
