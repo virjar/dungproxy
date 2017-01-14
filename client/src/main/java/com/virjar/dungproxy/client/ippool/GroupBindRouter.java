253cbde20da29376af4fb96b11c6210bf7481241
virjar <weijia.deng@qunar.com>
2017-01-14 22:47:17 +0800

    引入包错误,生产环境会出现classNotFond的问题

--- client/src/main/java/com/virjar/dungproxy/client/ippool/GroupBindRouter.java
+++ client/src/main/java/com/virjar/dungproxy/client/ippool/GroupBindRouter.java
@@ -2,7 +2,7 @@ package com.virjar.dungproxy.client.ippool;
 
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
